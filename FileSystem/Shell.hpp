4686f5ee84ecdc940065923e8f5fae74e8bcd57d
karlion <maksim.mykhaylenko@gmail.com>
2018-06-06 11:20:02 +0300

    small fix to shell

--- FileSystem/Shell.hpp
+++ FileSystem/Shell.hpp
@@ -63,37 +63,7 @@ void shell()
 
 		}
 		else if (temp == "in"){
-			int i;
-			int no_cyl;
-			int no_surf;
-			int no_sect;
-			int sect_len;
-			std::string disk_cont;
-			std::string newTemp;
-			for (i = 3; i < command.size(); i++){
-				if (command.at(i) == ' ') break;
-				newTemp += command.at(i);
-			}
-			no_cyl = stoi(newTemp);
-			newTemp.clear();
-			while (++i < command.size()){
-				if (command.at(i) == ' ') break;
-				newTemp += command.at(i);
-			}
-			no_surf = stoi(newTemp);
-			newTemp.clear();
-			while (++i < command.size()){
-				if (command.at(i) == ' ') break;
-				newTemp += command.at(i);
-			}
-			no_sect = stoi(newTemp);
-			newTemp.clear();
-			while (++i < command.size()){
-				if (command.at(i) == ' ') break;
-				newTemp += command.at(i);
-			}
-			sect_len = stoi(newTemp);
-			disk_cont = command.substr(++i, command.size() - 1);
+			std::string name = command.substr(3, command.size() - 1);
 		}
 		else if (temp == "sv"){
 			std::string name = command.substr(3, command.size() - 1);
